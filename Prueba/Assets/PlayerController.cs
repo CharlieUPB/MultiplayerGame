ec9fb423982207e38cd2df4b215c30435a58daa1
Elionor Cordova <elicg2012@hotmail.com>
2018-09-13 19:38:19 -0400

    Prueba: making the shooting function able for networking.

--- Prueba/Assets/PlayerController.cs
+++ Prueba/Assets/PlayerController.cs
@@ -8,10 +8,10 @@ public class PlayerController : NetworkBehaviour
 
     void Update()
     {
-		if (!isLocalPlayer)
-		{
-			return;
-		}
+        if (!isLocalPlayer)
+        {
+            return;
+        }
 
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
@@ -19,13 +19,16 @@ public class PlayerController : NetworkBehaviour
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Fire();
+            CmdFire();
         }
     }
 
-    void Fire()
+    // This [Command] code is called on the Client …
+    // … but it is run on the Server!
+    [Command]
+    void CmdFire()
     {
         // Create the Bullet from the Bullet Prefab
         var bullet = (GameObject)Instantiate(
@@ -36,12 +39,15 @@ public class PlayerController : NetworkBehaviour
         // Add velocity to the bullet
         bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
 
+        // Spawn the bullet on the Clients
+        NetworkServer.Spawn(bullet);
+
         // Destroy the bullet after 2 seconds
-        Destroy(bullet, 2.0f);        
+        Destroy(bullet, 2.0f);
     }
 
-	public override void OnStartLocalPlayer()
-	{
+    public override void OnStartLocalPlayer ()
+    {
         GetComponent<MeshRenderer>().material.color = Color.blue;
     }
 }
