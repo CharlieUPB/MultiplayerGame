b147f900a2c6a0e89a037ba44674d4dcce4ee125
Carlos Gamboa Encinas <carlo-x-17@hotmail.com>
2018-09-14 21:09:29 -0400

    Prueba: Fixed some bugs in the shooting.

--- Prueba/Assets/Scripts/Health.cs
+++ Prueba/Assets/Scripts/Health.cs
@@ -1,49 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
+using System.Collections;
 
 public class Health : NetworkBehaviour {
 
-	public const int maxHealth = 100;
-	[SyncVar (hook = "OnChangeHealth")] public int currentHealth = maxHealth;
-	public RectTransform healthbar;
+    public const int maxHealth = 100;
 
-	// Use this for initialization
-	void Start () {}
-	
-	// Update is called once per frame
-	void Update () {}
+    [SyncVar(hook = "OnChangeHealth")]
+    public int currentHealth = maxHealth;
 
-	public void TakeDamage(int amount) 
-	{
+    public RectTransform healthBar;
 
-		if(!isServer)
-		{
-			return;
-		}
+    public void TakeDamage(int amount)
+    {
+        if (!isServer)
+            return;
+        
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            currentHealth = maxHealth;
 
-		currentHealth -= amount;
-		if(currentHealth <= 0)
-		{
-			currentHealth = maxHealth;
-			RpcRespawn();
-		}
-	}
-	
-	void OnChangeHealth(int health)
-	{
-		healthbar.sizeDelta = new Vector2(health * 2, healthbar.sizeDelta.y);
-	}
+            // called on the Server, but invoked on the Clients
+            RpcRespawn();
+        }
+    }
 
-	[ClientRpc]
-	void RpcRespawn() 
-	{
-		if(isLocalPlayer)
-		{
-			transform.position = Vector3.zero;
-		}
-	}
+    void OnChangeHealth (int currentHealth )
+    {
+        healthBar.sizeDelta = new Vector2(currentHealth , healthBar.sizeDelta.y);
+    }
 
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if (isLocalPlayer)
+        {
+            // move back to zero location
+            transform.position = Vector3.zero;
+        }
+    }
 }
