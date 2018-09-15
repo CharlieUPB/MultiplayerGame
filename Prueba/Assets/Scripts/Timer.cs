16c1c6620c66291d01fee131b561498dcdfe20f8
Carlos Gamboa Encinas <carlo-x-17@hotmail.com>
2018-09-15 19:11:37 -0400

    Prueba: Added countdown timer.

---
+++ Prueba/Assets/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Timer : MonoBehaviour {
+
+	public Text timerText;
+	public float startTime = 0.0f;
+	
+	private int minutes;
+	private int seconds;
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		startTime -= Time.deltaTime;
+		minutes = (int)(startTime / 60);
+		seconds = (int)(startTime - (60*minutes));
+		timerText.text = "Time left: " + minutes + ":" + seconds;
+	}
+}
