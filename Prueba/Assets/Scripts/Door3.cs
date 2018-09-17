f63729e19d7c32684fef617ff62de607fd51175b
Carlos Gamboa Encinas <carlo-x-17@hotmail.com>
2018-09-16 20:18:02 -0400

    dev: Added main scenary.

---
+++ Prueba/Assets/Scripts/Door3.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door3 : MonoBehaviour {
+
+	Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		animator.SetBool("character_nearby",true);
+	}
+	void OnTriggerExit(Collider other) {
+		animator.SetBool("character_nearby",false);
+	}
+}
